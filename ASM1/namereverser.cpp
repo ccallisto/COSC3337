c7907bcba5c42f8aa04eba7c99b83c7919c04170
ccallisto <gavinsingleton23@gmail.com>
2024-09-07 16:16:57 -0500

    some other stuff

--- ASM1/namereverser.cpp
+++ ASM1/namereverser.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
 #include <fstream>
-
+//update this to include steps A-D, comment where they are specifically
 using namespace std;
 int main(){
-    fstream outfile;
+    fstream file1;
+    fstream file2;
     fstream infile;
     string input;
     
     infile.open("names_list_no_comma.txt", ios::in);
-    outfile.open("outputfile.txt", ios::out | ios::trunc);
+    file1.open("file1.txt", ios::out | ios::trunc);
+    file2.open("file2.txt", ios::out | ios::trunc);
     //cout << "Please input names to be reversed\n";
     while(!infile.fail()){
     
     getline(infile, input);      //using this over cin since by default it stops with whitespace   
 
-    int i = input.size();
-
-    for(i = input.size()-1; i >=0; i--)//initial name reverser
+    for(int i = input.size()-1; i >=0; i--)//initial name reverser
     {
 
         // cout << input[i]; //debugging output
-        outfile << input[i];
+        file1 << input[i];
     }                   
-    cout << "\n";
+    file1 << "\n";
+    
     }
-    outfile.close();
+
+    while(!infile.fail()){
+        getline(file1, input);
+        for(int i = input.size()-1; i >=0; i--)//secondary name re-reverser
+        {
+            // cout << input[i]; //debugging output
+            file2 << input[i];
+        }                   
+        file2 << "\n";
+    }    
+    file1.close();
+    file2.close();
     infile.close();
 }                   
